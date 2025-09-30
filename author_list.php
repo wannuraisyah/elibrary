e01a1ad6e63db75d1307fb3123fc675e4e4eff74
wan-aisyah <71620496+wannuraisyah@users.noreply.github.com>
2025-09-30 16:25:57 +0800

    Update author_list.php

--- author_list.php
+++ author_list.php
@@ -1,7 +1,6 @@
 <?php
     include('config.php');
 
-    // get all writers from database
     $query = "SELECT * FROM writer";
     $result = mysqli_query($con, $query);
 ?>
@@ -10,9 +9,13 @@
     <title>index test</title>
     <style>
         <?php include('style.css'); ?>
+
+        h1 {
+            font-family: lucida handwriting;
+        }
     </style>
 </head>
-<body class="container">
+<body class="container2">
     <center>
         <h1>Welcome to Your Library</h1>
         <h2>&#127775; LIST of AUTHORS &#127775;</h2>
@@ -32,7 +35,6 @@
         <div class="card-container">
             <?php while($res = mysqli_fetch_assoc($result)) { ?>
                 <div class="card2">
-                    <!-- If you have cover image column in DB -->
                     <div class="header">
                         <center><small><?php echo htmlspecialchars($res['id']); ?></small><br>
                         <?php echo htmlspecialchars($res['author']); ?><br></center>
@@ -43,4 +45,6 @@
         </div>
     </center>
 </body>
+<br>
+<footer><center><h2>&copy; WNA</h2></center></footer>
 </html>
